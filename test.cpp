099b13a4a984b2794322c9595ed95180ece95876
Minibruks <35571845+Minibruks@users.noreply.github.com>
2021-09-09 21:39:35 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "quadro_solve.cpp"
+#define ANSWER_SIZE 256
+
+/**
+    \brief The main function that imports quadro_solve and displays the value of each of the roots
+*/
+
+int main()
+{
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+
+    do
+    {
+        printf("Введите три числа:\n");
+        fflush(stdin);
+    } while (scanf("%lf %lf %lf", &a, &b, &c) != 3);
+
+    int res = quadroSolve(a, b, c, &x1, &x2);
+
+    switch (res)
+    {
+        case -1:
+            printf("Бесконечное множество решений\n");
+            break;
+
+        case 0:
+            printf("Действительных корней не существует\n");
+            break;
+
+        case 1:
+            printf("x = %lf\n", x1);
+            break;
+
+        case 2:
+            printf("x1 = %lf\nx2 = %lf\n", x1, x2);
+            break;
+
+        default:
+            printf("Ошибка. Не удалось найти значения корней\n");
+            return 1;
+    }
+
+    return 0;
+}
