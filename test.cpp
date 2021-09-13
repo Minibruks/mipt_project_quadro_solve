ad1d00970d24eb56b28d66db12b31189205dd104
Minibruks <35571845+Minibruks@users.noreply.github.com>
2021-09-13 13:40:21 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,27 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "quadro_solve.cpp"
-#define ANSWER_SIZE 256
-
 /**
-    \brief The main function that imports quadro_solve and displays the value of each of the roots
+    \brief The program implements a check of the function operation quadro_solve.cpp
 */
 
-int main()
-{
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "quadro_solve.cpp"
 
-    do
-    {
-        printf("Введите три числа:\n");
-        fflush(stdin);
-    } while (scanf("%lf %lf %lf", &a, &b, &c) != 3);
+/**
+ * @brief A funtion for reading values from terminal
+ * @param a
+ * @param b
+ * @param c
+ * @return Number of input values
+ */
 
-    int res = quadroSolve(a, b, c, &x1, &x2);
+int inputVal(double* a, double* b, double* c) {
+    int input_count = scanf("%lg %lg %lg", a, b, c);
+    return input_count;
+}
 
-    switch (res)
-    {
+/**
+ * @brief A function that prints number of solutions and their values
+ * @param num_of_solutions
+ * @param x1
+ * @param x2
+ * @return Number of solutions and their values
+ */
+int printNumOfSol(int num_of_solutions, double* x1, double* x2) {
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+
+    switch (num_of_solutions) {
         case -1:
             printf("Бесконечное множество решений\n");
             break;
@@ -31,17 +41,33 @@ int main()
             break;
 
         case 1:
-            printf("x = %lf\n", x1);
+            printf("x = %lg\n", *x1);
             break;
 
         case 2:
-            printf("x1 = %lf\nx2 = %lf\n", x1, x2);
+            printf("x1 = %lg\nx2 = %lg\n", *x1, *x2);
             break;
 
         default:
             printf("Ошибка. Не удалось найти значения корней\n");
             return 1;
     }
+    return 0;
+}
+
+int main() {
+    double a = 0, b = 0, c = 0;
+    double x1 = 0, x2 = 0;
+
+    printf("Введите ТРИ числа:\n");
+    while (inputVal(&a, &b, &c) != 3) {
+        printf("Вы не ввели число\n");
+        scanf("%*s");
+    }
+
+    int num_of_solutions = solveQuadratic(a, b, c, &x1, &x2);
+
+    printNumOfSol(num_of_solutions, &x1, &x2);
 
     return 0;
 }
