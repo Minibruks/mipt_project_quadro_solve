b3e5ca6b7bd28543325b558fbadd54dfc2436b38
Minibruks <35571845+Minibruks@users.noreply.github.com>
2021-09-18 20:04:27 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,51 +1,55 @@
 /**
-    \brief The program implements a check of the function operation quadro_solve.cpp
+    @brief The program implements a check of the function operation quadro_solve.cpp
+    @version 1.0.0
 */
 
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include "quadro_solve.cpp"
 
 /**
- * @brief A funtion for reading values from terminal
+ * @brief A function for reading float values from terminal
+ * @version 1.0.0
  * @param a
  * @param b
  * @param c
- * @return Number of input values
+ * @return Nothing
  */
 
-int inputVal(double* a, double* b, double* c) {
-    int input_count = scanf("%lg %lg %lg", a, b, c);
-    return input_count;
+void readCoefficients(double* a, double* b, double* c) {
+    assert(a != NULL);
+    assert(b != NULL);
+    assert(c != NULL);
+
+    printf("Введите ТРИ числа:\n");
+    while (scanf("%lg %lg %lg", a, b, c) != 3) {
+        printf("Вы не ввели число\n");
+        scanf("%*s");
+    }
 }
 
 /**
  * @brief A function that prints number of solutions and their values
+ * @version 1.0.0
  * @param num_of_solutions
  * @param x1
  * @param x2
  * @return Number of solutions and their values
  */
-int printNumOfSol(int num_of_solutions, double* x1, double* x2) {
-    assert(x1 != NULL);
-    assert(x2 != NULL);
-
+int printSolutions(NumOfSolutions num_of_solutions, double x1, double x2) {
     switch (num_of_solutions) {
-        case -1:
+        case INF_SOLUTIONS:
             printf("Бесконечное множество решений\n");
             break;
 
-        case 0:
+        case ZERO_SOLUTIONS:
             printf("Действительных корней не существует\n");
             break;
 
-        case 1:
-            printf("x = %lg\n", *x1);
+        case ONE_SOLUTION:
+            printf("x = %lg\n", x1);
             break;
 
-        case 2:
-            printf("x1 = %lg\nx2 = %lg\n", *x1, *x2);
+        case TWO_SOLUTIONS:
+            printf("x1 = %lg\nx2 = %lg\n", x1, x2);
             break;
 
         default:
@@ -57,17 +61,13 @@ int printNumOfSol(int num_of_solutions, double* x1, double* x2) {
 
 int main() {
     double a = 0, b = 0, c = 0;
+
+    readCoefficients(&a, &b, &c);
+
     double x1 = 0, x2 = 0;
+    NumOfSolutions num_of_solutions = solveQuadratic(a, b, c, &x1, &x2);
 
-    printf("Введите ТРИ числа:\n");
-    while (inputVal(&a, &b, &c) != 3) {
-        printf("Вы не ввели число\n");
-        scanf("%*s");
-    }
-
-    int num_of_solutions = solveQuadratic(a, b, c, &x1, &x2);
-
-    printNumOfSol(num_of_solutions, &x1, &x2);
+    printSolutions(num_of_solutions, x1, x2);
 
     return 0;
 }
